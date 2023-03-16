1c27b647b569a5cacf0624ee3a7f6bd4b24bb106
Mason Bercier <masonbercier@yahoo.com>
2023-03-16 14:21:57 -0700

    added gif sprite

--- jshw3.js
+++ jshw3.js
@@ -21,13 +21,14 @@ async function pokemonData(name){
     const defense_stat = data['stats'][2]['base_stat']
     const primary_type = data['types'][0]['type']['name']
     const sprite_url = data['sprites']['front_default']
+    const gif_sprite_url = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
 
     const pokeDiv = document.querySelector('.poke-div')
     pokeDiv.innerHTML = `
         <div class="card mx-auto text-bg bg-dark"  style="width: 18rem;">
             <h5 class="card-title text-white">${data.name}</h5>
             <h5 class="card-title text-white">HP: ${hp_stat}</h5>
-            <img class="card-img-top" src="${sprite_url}" alt="Card image cap">
+            <img class="card-img-top" src="${gif_sprite_url}" alt="Card image cap">
             <ul class="list-group list-group-flush">
             <li class="list-group-item text-white bg-dark">Type: ${primary_type}</li>
             <li class="list-group-item text-white bg-dark">Ability: ${pokeAbility}</li>
